82db010a66efd122a3c38c096f9ace9e50c76090
kellyannclark <103697379+kellyannclark@users.noreply.github.com>
2024-02-19 18:23:37 -0700

    Add files via upload

---
+++ Data/RazorPagesMovieContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models; // Adjust the namespace if your 'Movie' model is in a different namespace
+
+namespace RazorPagesMovie.Data
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        public RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
+
